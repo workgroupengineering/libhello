7b7176401b409e000c4b91bbee8c35aa89ffd3ce
workgroupengineering <workgroupengineering@users.noreply.github.com>
2023-09-11 15:27:11 +0200

    Initial commit

---
+++ src/lib/hello.c
@@ -0,0 +1,10 @@
+#include "hello.h"
+void HelloFunc()
+{
+    printf("Hello World\n");
+}
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ src/lib/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+
+void HelloFunc();
+int add(int a, int b);
+#endif

e80ea657d20c51fb94f0e1ca60284f7fbc89fc0f
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-12-05 11:16:50 +0100

    Revert "only libpng dynamic"
    This reverts commit 85f6a9471068be02823ff28bfecbcd66d7c173e4.

--- overlay/trypletes/arm64-windows-shared.cmake
+++ overlay/trypletes/arm64-windows-shared.cmake
@@ -1,7 +1,3 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-if(PORT MATCHES "libpng")
-    set(VCPKG_CRT_LINKAGE dynamic)
-endif()
--- overlay/trypletes/x64-windows-shared.cmake
+++ overlay/trypletes/x64-windows-shared.cmake
@@ -1,7 +1,4 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
-if(PORT MATCHES "libpng")
-    set(VCPKG_CRT_LINKAGE dynamic)
-endif()

2d3fd2aebf9ed405e9df1818c7924e3b3dbd582d
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-12-05 10:38:24 +0100

    only libpng dynamic

--- overlay/trypletes/arm64-linux-shared.cmake
+++ overlay/trypletes/arm64-linux-shared.cmake
@@ -1,8 +1,12 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
 
 set(PNG_EXECUTABLES OFF)
+
+if(PORT EQUAL "libpng")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
--- overlay/trypletes/arm64-windows-shared.cmake
+++ overlay/trypletes/arm64-windows-shared.cmake
@@ -1,3 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
+
+if(PORT EQUAL "libpng")
+    set(VCPKG_CRT_LINKAGE dynamic)
+endif()

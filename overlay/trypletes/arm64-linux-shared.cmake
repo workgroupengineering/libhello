6f2dfeee1429490fde2275c0d0b2b74d559ea8e9
Giuseppe Lippolis <workgroupengineering@outlook.com>
2023-12-05 12:53:49 +0100

    revert

--- overlay/trypletes/arm64-linux-shared.cmake
+++ overlay/trypletes/arm64-linux-shared.cmake
@@ -1,14 +1,8 @@
 set(VCPKG_TARGET_ARCHITECTURE arm64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
 
 set(PNG_EXECUTABLES OFF)
-
-if(PORT EQUAL "freetype")
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-endif()
-
-message("Port; $PORT $VCPKG_LIBRARY_LINKAGE")
